518e4ff9e53036ae6e871852fbd3ebf5308f977a
Stanislav <swatogor@yandex.ru>
2019-06-08 12:25:06 +0300

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,26 @@
+'''
+Дек - это упорядоченная коллекция элементов, подобная очереди. 
+Новые элементы могут быть добавлены как в голову, так и в хвост
+'''
+class Deque:
+
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+        
+    def addFront(self, item):
+        self.items.append(item)
+    
+    def addRear(self, item):
+        self.items.insert(0,item)
+        
+    def removeFront(self):
+        return self.items.pop()
+        
+    def removeRear(self):
+        return self.items.pop(0)
+        
+    def size(self):
+        return len(self.items)
